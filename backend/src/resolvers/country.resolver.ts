79664a88b92678d29570aae6fc516d6799db207e
Charlie FEIX <charlie@onicorn.dev>
2024-04-15 10:59:38 +0200

    🗃️ Added mutation to add a country

--- backend/src/resolvers/country.resolver.ts
+++ backend/src/resolvers/country.resolver.ts
@@ -1,11 +1,19 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import * as CountryService from "../services/country.service";
 import { Country } from "../entities/country";
+import { CreateCountryInputType } from "../types/CreateCountryInputType";
 
 @Resolver(Country)
 export class CountryResolver {
     @Query(() => [Country])
     countries(): Promise<Country[]> {
         return CountryService.getAll();
+    }
+
+    @Mutation(() => Country)
+    createCountry(
+        @Arg("country") country: CreateCountryInputType
+    ): Promise<Country> {
+        return CountryService.create({ ...country });
     }
 }
--- backend/src/services/country.service.ts
+++ backend/src/services/country.service.ts
@@ -3,3 +3,8 @@ import { Country } from "../entities/country";
 export function getAll(): Promise<Country[]> {
     return Country.find();
 }
+
+export function create(data: { code: string; name: string; emoji: string }): Promise<Country> {
+    const country = new Country(data);
+    return country.save();
+}
